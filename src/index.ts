14a4a01c6c630e0ad519f0964170bebe32f77179
Jacob Gantt <gantt42@gmail.com>
2022-06-29 02:03:11 -0400

    Bug fixes for manual crash

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ var async = require('async');
 var fs = require('graceful-fs').promises;
 const log4js = require("log4js");
 
+exports.isAlive = async () => {
+    return true;
+}
+
 exports.start = async () => {
     const APP_NAME = "jgantts-website";
     const PUBLIC_DIR = path.join(path.dirname(await fs.realpath(__filename)), 'PUBLIC');
@@ -31,82 +35,85 @@ exports.start = async () => {
     app.listen(PORT_HTTP, listenResponse);
     app.listen(PORT_HTTPS, listenResponse);
 
-    app.use((req, res, next) => {
+    app.use(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
         let path = req.url;
         logger.debug(`Request${path}`);
-        try {
-            next()
-        } catch (e) {
-            let err = e as Error
-            if (err) {
-                logger.debug(`Request${err.message}`);
-            }
+        if (path[path.length - 1] !== '/') {
+            req.url = path + '/';
         }
-        res.end();
-        cluster.worker.kill();
+        next();
     })
 
     app.get('/admin/*', async (req: express.Request, res: express.Response) => {
         let path = req.url;
 
-        if (path = "/admin/error/crash") {
-            logger.debug("Admin effected crash.");
-            throw new Error("Admin effected crash.");
+        if (path === "/admin/error/crash/") {
+            logger.debug("Admin-effected crash.");
+            cluster.worker.kill();
+        } else if (path === "/admin/error/500/") {
+            res.writeHead(500, {'Content-Type': 'text/html'});
+            res.write("<p>Hello, Admin</p>");
+            res.write(`<p>Here's your ${'500'} error.</p>`);
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write("<p>Hello, Admin</p>");
+            res.write(`<p>Here's your ${'200'} response.</p>`);
         }
-        res.writeHead(500, {'Content-Type': 'text/html'});
-        res.write("<p>Hello, Admin</p>");
+        res.end();
     });
 
     app.get('/*', async (req: express.Request, res: express.Response) => {
+        let query = url.parse(req.url, true);
+        let fileName = path.join(PUBLIC_DIR, query.pathname);
+        if(path.parse(fileName).ext === "") {
+            fileName += 'index.html'
+        }
+        fileName = path.resolve(fileName);
         try {
-            let query = url.parse(req.url, true);
-            let fileName = path.join(PUBLIC_DIR, query.pathname);
-            if(path.parse(fileName).ext === "") {
-                fileName += 'index.html'
+            let contents = await fs.readFile(fileName);
+            let contentType: string
+            switch(path.parse(fileName).ext) {
+                case '.html': contentType = 'text/html';
+                break;
+
+                case '.css': contentType = 'text/css';
+                break;
+
+                case '.js': contentType = 'application/javascript';
+                break;
+
+                case '.gif': contentType = 'image/gif';
+                break;
+
+                case '.jpeg':
+                case '.jpg': contentType = 'image/jpeg';
+                break;
+
+                case '.png': contentType = 'image/png';
+                break;
+
+                case '.svg': contentType = 'image/svg+xml';
+                break;
+
+                case '.pdf': contentType = 'application/pdf';
+                break;
+
+                default: throw Error('Unrecognized file type');
+                break;
             }
-            fileName = path.resolve(fileName);
-            try {
-                let contents = await fs.readFile(fileName);
-                let contentType: string
-                switch(path.parse(fileName).ext) {
-                    case '.html': contentType = 'text/html';
-                    break;
-
-                    case '.css': contentType = 'text/css';
-                    break;
-
-                    case '.js': contentType = 'application/javascript';
-                    break;
-
-                    case '.gif': contentType = 'image/gif';
-                    break;
-
-                    case '.jpeg':
-                    case '.jpg': contentType = 'image/jpeg';
-                    break;
-
-                    case '.png': contentType = 'image/png';
-                    break;
-
-                    case '.svg': contentType = 'image/svg+xml';
-                    break;
-
-                    case '.pdf': contentType = 'application/pdf';
-                    break;
-
-                    default: throw Error('Unrecognized file type');
-                    break;
-                }
-                res.writeHead(200, {'Content-Type': contentType});
-                res.write(contents);
-            } catch (err) {
+            console.log("writing head2");
+            res.writeHead(200, {'Content-Type': contentType});
+            res.write(contents);
+        } catch (e) {
+            let err = e as Error
+            if (err) {
+                console.log(err.message)
+                console.log("writing head3");
                 res.writeHead(500, {'Content-Type': 'text/html'});
                 res.write("<p>500 - It's not you, it's us.</p>");
                 logger.debug(JSON.stringify(fileName));
                 logger.debug(err);
             }
-        } catch (err) {
-
         }
         res.end();
     });

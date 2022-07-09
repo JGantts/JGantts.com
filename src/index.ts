c06d1426ab9e448bad8f96b61679449fc63c9f2d
Jacob Gantt <gantt42@gmail.com>
2022-07-08 21:43:15 -0400

    Add ref to ssl authority verification file

--- src/index.ts
+++ src/index.ts
@@ -85,6 +85,14 @@ exports.start = async () => {
 
     logger.debug(`NODE_SITE_PUB_ENV: ${process.env.NODE_SITE_PUB_ENV}`);
 
+    app.get('/.well-known/pki-validation/7E166EFD1CA6C0A1EC8452A62671C9DC.txt', async function(req, res) {
+        let contents = await fs.readFile(`/keys/jgantts.com/7E166EFD1CA6C0A1EC8452A62671C9DC.txt`);
+        res.writeHead(200, {'Content-Type': "text/plain"});
+        res.write(contents);
+        res.end()
+    })
+
+
     app.get('/admin*', async (req: express.Request, res: express.Response) => {
         let reqUrl = req.url;
 

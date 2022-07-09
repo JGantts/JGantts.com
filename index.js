6170a710c38480411c7d9ce9c8dabc47b4a898de
Jacob Gantt <gantt42@gmail.com>
2022-07-09 14:46:05 -0400

    Add proxy for port 80

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const log4js = require('log4js');
 const website = require('./dist/out/index.js');
+const express = require('express');
+const http = require('http');
+const httpProxy = require('http-proxy');
 
 const APP_NAME = "jgantts-website-local-publisher";
+
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
 
 process.env.NODE_SITE_PUB_ENV = 'dev';
 
 log4js.configure({
-  appenders: { publish: { type: "file", filename: `${APP_NAME}.log` } },
-  categories: { default: { appenders: ["publish"], level: "error" } }
+    appenders: { publish: { type: "file", filename: `${APP_NAME}.log` } },
+    categories: { default: { appenders: ["publish"], level: "error" } }
 });
 const logger = log4js.getLogger();
 logger.level = "debug";
 
-logger.debug("Begin Log");
+async function launch() {
+    logger.debug("Begin Log");
 
-logger.debug(`Node Site #${process.pid} starting`);
-website.start();
-logger.debug(`Node Site #${process.pid} started`);
+    await website.start();
+
+    let port = await website.port();
+
+    httpProxy.createProxyServer({target: `http://localhost:${port}`}).listen(HTTP_PORT);
+
+    logger.debug(`Node Site #${process.pid} started @${port}`);
+}
+
+launch();

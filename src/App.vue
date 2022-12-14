905736007b16b809b27f811d609d6d660d6a8cee
gantt42 <gantt42@gmail.com>
2022-12-14 15:37:14 -0500

    hmmmmm

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
-<!--
-Here we show the simplest possible component which accepts a prop and renders it.
-Learn more about components in the guide!
--->
-
-<script setup>
-import { ref } from 'vue'
-import TodoItem from './TodoItem.vue'
-
-const groceryList = ref([
-  { id: 0, text: 'Vegetables' },
-  { id: 1, text: 'Cheese' },
-  { id: 2, text: 'Whatever else humans are supposed to eat' }
-])
-</script>
-
 <template>
-  <ol>
-    <!--
-      We are providing each todo-item with the todo object
-      it's representing, so that its content can be dynamic.
-      We also need to provide each component with a "key",
-      which is explained in the guide section on v-for.
-    -->
-    <TodoItem
-      v-for="item in groceryList"
-      :todo="item"
-      :key="item.id"
-    ></TodoItem>
-  </ol>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
 </template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

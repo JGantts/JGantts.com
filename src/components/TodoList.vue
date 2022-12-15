c4a8b0af3482d0759f97081bc303440ba84d2f74
gantt42 <gantt42@gmail.com>
2022-12-15 17:37:42 -0500

    components -> options

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,15 +2,26 @@
 Here we show the simplest possible component which accepts a prop and renders it.
 Learn more about components in the guide!
 -->
+
 <script setup>
-import { ref } from 'vue'
 import TodoItem from './TodoItem.vue'
-const groceryList = ref([
-  { id: 0, text: 'Vegetables' },
-  { id: 1, text: 'Cheese' },
-  { id: 2, text: 'Whatever else humans are supposed to eat' }
-])
+
+export default {
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+  }
+}
 </script>
+
 <template>
   <ol>
     <!--
@@ -21,6 +32,8 @@ const groceryList = ref([
     -->
     <TodoItem
       v-for="item in groceryList"
+      :todo="item"
+      :key="item.id"
     ></TodoItem>
   </ol>
 </template>

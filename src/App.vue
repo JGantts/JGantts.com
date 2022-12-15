94a030e49b13cae9f85ebca9556dd0314d1f5966
gantt42 <gantt42@gmail.com>
2022-12-15 18:36:22 -0500

    Probly not

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@
     <router-view/>
   </div>
 </template>
-<script>
-export default {
-  name: 'App'
-}
+
+<script setup>
+  const name = 'App'
 </script>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;

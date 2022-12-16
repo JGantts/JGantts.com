f177a35e752a548647cc40e3635f8542ef5ccfb0
gantt42 <gantt42@gmail.com>
2022-12-16 17:07:06 -0500

    hi

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -16,18 +16,12 @@ const groceryList = ref([
 
 <template>
   <ol>
-    <!--
-      We are providing each todo-item with the todo object
-      it's representing, so that its content can be dynamic.
-      We also need to provide each component with a "key",
-      which is explained in the guide section on v-for.
-    -->
     <p>Item 1</p>
-    <TodoItem
+    <P
       v-for="item in groceryList"
       :todo="item"
       :key="item.id"
-    ></TodoItem>
+    >Hi</p>
     <p>Item n</p>
   </ol>
 </template>

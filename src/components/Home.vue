323cc417c89578add9dd7cca993da46fdeba035b
Jacob Gantt <gantt42@gmail.com>
2022-12-17 17:54:54 -0500

    Create Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+  <div id="main">
+    <div class="inner">
+      <div id="container01" class="container default full">
+        <div class="wrapper">
+          <div class="inner">
+            <h1 id="text01">JGantts.com</h1>
+            <p id="text02">Welcome, all.</p>
+          </div>
+        </div>
+      </div>
+      <div id="container02" class="container columns full">
+        <div class="wrapper">
+          <div class="inner">
+            <div>
+              <p id="text03">Contact me about software:</p>
+              <p id="text04">Jacob Gantt</p>
+              <ul id="links01" class="links">
+                <li class="n01">
+                  <a href="mailto:gantt42@gmail.com">gantt42@gmail.com</a>
+                </li><li class="n02">
+                  <a href="https://github.com/JGantts">github.com/JGantts</a>
+                </li><li class="n03">
+                  <a href="https://jgantts.com/resume/GanttJ-Resume.pdf">GanttJ-Resume.pdf</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p id="text06"><span class="p">Looking for clients and employers.<br />I&#039;m a developer with years of professional and personal experience; from improving enterprise solutions to fuzz testing code parsers; game mods, websites; APIs and UX; C#, JavaScript; Swift, Laravel; I love it all!</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="container03" class="container default full">
+        <div class="wrapper">
+          <div class="inner">
+            <p id="text05">I write software!</p>
+            <p id="text07">© 2022 Jacob Gantt</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

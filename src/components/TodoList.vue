0af93307a456a6ca848a23287120ea0cb0705efe
gantt42 <gantt42@gmail.com>
2022-12-17 17:19:10 -0500

    Give up on composition api

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,15 +3,23 @@ Here we show the simplest possible component which accepts a prop and renders it
 Learn more about components in the guide!
 -->
 
-<script setup>
-import { ref } from 'vue'
+<script>
 import TodoItem from './TodoItem.vue'
 
-const groceryList = ref([
-  { id: 0, text: 'Vegetables' },
-  { id: 1, text: 'Cheese' },
-  { id: 2, text: 'Whatever else humans are supposed to eat' }
-])
+export default {
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+  }
+}
 </script>
 
 <template>

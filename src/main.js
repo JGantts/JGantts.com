85a883353fb0fe0cdd1aaceca8df02f12a6cf5d9
Jacob Gantt <gantt42@gmail.com>
2022-12-17 20:48:31 -0500

    !!!

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import '../src/assets/css/styles.scss';
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

78217ad21d36f0f33a647a2fc18b370004631513
gantt42 <gantt42@gmail.com>
2022-12-19 12:41:33 -0500

    First "complete" attempt

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,5 +1,9 @@
 <script>
-let pixelSize = 10;
+import BackgroundBox from './BackgroundBox.vue';
+import Vue from 'vue';
+var BoxClass = Vue.extend(BackgroundBox);
+
+let boxSize = 10;
 
 export default {
   data() {
@@ -11,40 +15,59 @@ export default {
       }
     };
   },
+
   methods: {
     async resizedWindow() {
-      let newWidthRaw = window.outerHeight/pixelSize;
+      let newWidthRaw = window.outerWidth/boxSize;
       let newWidthPerSideRaw = newWidthRaw/2;
       let newPixelsPerSide = Math.ceil(newWidthPerSideRaw);
+      let oldPixelsPerSide = this.topRowBoxes.left.length;
+      let countToAdd = newPixelsPerSide - oldPixelsPerSide;
 
-      if (newPixelsPerSide = 0) {
+      if (countToAdd = 0) {
         return;
 
-      } else if(newPixelsPerSide < 0) {
+      } else if(countToAdd < 0) {
         //Subtract boxes
-        let countToDelete = Math.abs(newPixelsPerSide);
+        let countToDelete = Math.abs(countToAdd);
         this.topRowBoxes.left.splice(this.topRowBoxes.left.length - countToDelete, countToDelete);
         this.topRowBoxes.right.splice(this.topRowBoxes.left.length - countToDelete, countToDelete);
 
-      } else if(newPixelsPerSide > 0) {
+      } else if(countToAdd > 0) {
         //Add boxes
-        for(side in [this.topRowBoxes.left, this.topRowBoxes.right]) {
+        for(let side in [this.topRowBoxes.left, this.topRowBoxes.right]) {
           [...Array(newPixelsPerSide)].forEach((v, i) => {
             side.push(
-              [ ]
+              {
+                spawnInterval: Math.floor(Math.random()*100),
+                spawnCountdown: 0,
+                boxes: [ ],
+              }
             );
           })
         }
+        if (oldPixelsPerSide === 0) {
+          let slowOnes = getRandomElements(side, 3);
+          for(let slowOne in slowOnes) {
+            slowOne.spawnInterval = Math.floor(Math.random()*10) + 90;
+          }
+          let fastOnes = getRandomElements(side, 3);
+          for(let fastOne in fastOnes) {
+            fastOne.spawnInterval = Math.floor(Math.random()*10);
+          }
+        }
       }
     },
+
     async renderLoop() {
       await Promise.all(
         new Promise(r => setTimeout(r, 50)),
-        this.render(),
+        this.renderScene(),
       );
       this.renderLoop();
     },
-    async render() {
+
+    async renderScene() {
       for(sideAndDirection in [
         {side: this.topRowBoxes.left, dir: -1},
         {side: this.topRowBoxes.right, dir: 1}
@@ -52,25 +75,74 @@ export default {
         let side = sideAndDirection.side;
         let direction = sideAndDirection.dir;
         side.forEach(function (column, index) {
-          
+          this.renderColumn(column, direction*(index+1));
         });
       }
-    }
+    },
+
+    async renderColumn(column, xPosition) {
+      column.spawnCountdown += 1;
+      if(column.length == 0 || column.spawnCountdown >= column.spawnInterval) {
+        let position = { x: xPosition, y: column.boxes.length };
+        let color = { 
+          r: Math.floor(Math.random()*10) + 190, 
+          g: Math.floor(Math.random()*10) + 45, 
+          b: Math.floor(Math.random()*10) + 245,
+        };
+        column.push({
+          position: position,
+          color: color,
+          element: this.addBox(position, color),
+        });
+      }
+    },
+
+    addBox(position, color) {
+      var newBox = new BoxClass({
+        propsData: {
+          position: position,
+          color: color,
+        }
+      });
+      newBox.$mount();
+      this.$refs.container.appendChild(newBox.$el);
+      return newBox;
+    },
   },
+
   created() {
     window.addEventListener("resize", this.resizedWindow);
   },
+
   destroyed() {
     window.removeEventListener("resize", this.resizedWindow);
   },
+
   mounted() {
     console.log("Hello, world!");
-   this.resizedWindow();
+    this.resizedWindow();
   },
+}
+
+function getRandomElements(arr, n) {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
 }
 </script>
 
 <template>
-  <div id="animation-base">
+  <div id="animation-base" style="
+    position: absolute,
+    left: 50vw;
+  ">
   </div>
 </template>
---
+++ src/components/BackgroundBox.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  props: {
+    position: Object,
+    color: Object,
+  },
+  data() {
+    return {
+      boxSize: 10,
+    }
+  },
+  methods: {
+    xDirection() {
+      if(positons.x < 0) {
+        return -1;
+      }
+      return 1;
+    },
+  },
+}
+</script>
+
+<template>
+  <div :style="{
+    color: rgb(color.r, color.g, color.b),
+    position: absolute,
+    left: position.x*boxSize,
+    top: position.y*boxSize,
+  }">
+  </div>
+</template>

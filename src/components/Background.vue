7b0de2e3d44538aae8c5bcf570e19a0ddb5cdc20
gantt42 <gantt42@gmail.com>
2022-12-19 08:58:51 -0500

    WIP

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,13 +1,75 @@
 <script>
+let pixelSize = 10;
+
 export default {
-  async created() {
-   await new Promise(r => setTimeout(r, 1000));
+  data() {
+    return { 
+      topRowBoxes: {
+        countPerSide: 0,
+        left: [ ],
+        right: [ ],
+      }
+    };
+  },
+  methods: {
+    async resizedWindow() {
+      let newWidthRaw = window.outerHeight/pixelSize;
+      let newWidthPerSideRaw = newWidthRaw/2;
+      let newPixelsPerSide = Math.ceil(newWidthPerSideRaw);
+
+      if (newPixelsPerSide = 0) {
+        return;
+
+      } else if(newPixelsPerSide < 0) {
+        //Subtract boxes
+        let countToDelete = Math.abs(newPixelsPerSide);
+        this.topRowBoxes.left.splice(this.topRowBoxes.left.length - countToDelete, countToDelete);
+        this.topRowBoxes.right.splice(this.topRowBoxes.left.length - countToDelete, countToDelete);
+
+      } else if(newPixelsPerSide > 0) {
+        //Add boxes
+        for(side in [this.topRowBoxes.left, this.topRowBoxes.right]) {
+          [...Array(newPixelsPerSide)].forEach((_, _) => {
+            side.push(
+              [ ]
+            );
+          })
+        }
+      }
+    },
+    async renderLoop() {
+      await Promise.all(
+        new Promise(r => setTimeout(r, 50)),
+        this.render(),
+      );
+      this.renderLoop();
+    },
+    async render() {
+      for(sideAndDirection in [
+        {side: this.topRowBoxes.left, dir: -1},
+        {side: this.topRowBoxes.right, dir: 1}
+      ]) {
+        let side = sideAndDirection.side;
+        let direction = sideAndDirection.dir;
+        side.forEach(function (column, index) {
+          console.log('%d: %s', i, value);
+        })
+      }
+    }
+  },
+  created() {
+    window.addEventListener("resize", this.resizedWindow);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.resizedWindow);
+  },
+  mounted() {
+   this.resizedWindow();
   },
 }
 </script>
 
 <template>
-  <div>
-    Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background Background 
+  <div id="animation-base">
   </div>
 </template>

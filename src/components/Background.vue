f9510d063778ea10eddb52ff66b2dedd8a528497
gantt42 <gantt42@gmail.com>
2022-12-20 14:17:44 -0500

    try again

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -21,7 +21,7 @@ export default {
     async resizedWindow() {
       let newWidthRaw = window.outerWidth/boxSize;
       let newWidthPerSideRaw = newWidthRaw/2;
-      let newPixelsPerSide = Math.ceil(newWidthPerSideRaw) + 1;
+      let newPixelsPerSide = Math.ceil(newWidthPerSideRaw);
       let oldPixelsPerSide = this.topRowBoxes.left.length;
       let countToAdd = newPixelsPerSide - oldPixelsPerSide;
 
@@ -33,18 +33,6 @@ export default {
       } else if(countToAdd < 0) {
         //Subtract boxes
         let countToDelete = Math.abs(countToAdd);
-        for(let i=0; i< countToDelete; i++) {
-          let columnLeft = this.topRowBoxes[i];
-          for(let index in columnLeft.boxes) {
-            let toDelete = columnLeft.boxes[index].element;
-            toDelete.parentNode.removeChild(toDelete);
-          }
-          let columnRight = this.topRowBoxes[i];
-          for(let index in columnLeft.boxes) {
-            let toDelete = columnRight.boxes[index].element;
-            toDelete.parentNode.removeChild(toDelete);
-          }
-        }
         this.topRowBoxes.left.splice(this.topRowBoxes.left.length - countToDelete, countToDelete);
         this.topRowBoxes.right.splice(this.topRowBoxes.left.length - countToDelete, countToDelete);
 
@@ -55,8 +43,8 @@ export default {
           [...Array(countToAdd)].forEach((v, i) => {
             sides[sideIndex].push(
               {
-                spawnIncrement: Math.random()+0.5,
-                spawnCountdown: -Math.random()*100,
+                spawnInterval: Math.floor(Math.random()*100),
+                spawnCountdown: 0,
                 boxes: [ ],
               }
             );
@@ -64,11 +52,11 @@ export default {
           if (oldPixelsPerSide === 0) {
             let slowOnes = getRandomElements(sides[sideIndex], 3);
             for(let slowOneIndex in slowOnes) {
-              slowOnes[slowOneIndex].spawnIncrement = 0.5 + Math.random()/10;
+              slowOnes[slowOneIndex].spawnInterval = Math.floor(Math.random()*10) + 90;
             }
             let fastOnes = getRandomElements(sides[sideIndex], 3);
             for(let fastOneIndex in fastOnes) {
-              fastOnes[fastOneIndex].spawnIncrement = 1.5 - Math.random()/10;
+              fastOnes[fastOneIndex].spawnInterval = Math.floor(Math.random()*10);
             }
           }
         }
@@ -76,6 +64,7 @@ export default {
     },
 
     async renderLoop() {
+      console.log("render loop");
       await Promise.all([
         new Promise(r => setTimeout(r, 50)),
         this.renderScene(),
@@ -84,6 +73,7 @@ export default {
     },
 
     async renderScene() {
+      console.log("render scene");
       let sidesAndDirections = [
         {side: this.topRowBoxes.left, dir: -1},
         {side: this.topRowBoxes.right, dir: 1}
@@ -94,106 +84,24 @@ export default {
         //console.log(side);
         //console.log(JSON.stringify(side));
         for (let indexB in side) {
-          this.renderColumn(side, Number(indexB), side[indexB], direction*(Number(indexB)+1));
+          this.renderColumn(side[indexB], direction*(indexB+1));
         }
       }
     },
 
-    async renderColumn(side, sideIndex, column, xPosition) {
-      if(column.spawnCountdown < 0) {
-        column.spawnCountdown += 1;
-      } else {
-        column.spawnCountdown += column.spawnIncrement;
-      }
+    async renderColumn(column, xPosition) {
+      console.log("render column");
+      column.spawnCountdown += 1;
       if(
-        column.spawnCountdown >= column.spawnIncrement
-        && (column.boxes.length-1)*boxSize < window.outerHeight
+        (column.boxes.length == 0 || column.spawnCountdown >= column.spawnInterval)
+        && column.boxes.length*boxSize < window.outerHeight
       ) {
-        column.spawnCountdown = 0
-      
         let position = { x: xPosition, y: column.boxes.length };
-
         let color = { 
-          r: Math.floor(Math.random()*50) + 0,
-          g: Math.floor(Math.random()*50) + 100,
-          b: Math.floor(Math.random()*50) + 200,
-          a: Math.floor(Math.random()*200) + 25,
+          r: Math.floor(Math.random()*10) + 190, 
+          g: Math.floor(Math.random()*10) + 45, 
+          b: Math.floor(Math.random()*10) + 245,
         };
-        let parent = null;
-        let leftCousin = null;
-        let rightCousin = null;
-        
-        parent = column.boxes[column.boxes.length-1];
-        let leftLineage = side[sideIndex - 1];
-        if (leftLineage) {
-          leftCousin = leftLineage.boxes[column.boxes.length - 1]
-        }
-        let rightLineage = side[sideIndex + 1];
-        if (rightLineage) {
-          rightCousin = rightLineage.boxes[column.boxes.length - 1]
-        }
-        let colorToTint = {
-          r: 0,
-          g: 0,
-          b: 0,
-          a: 0
-        }
-        let colorsAdded = 0;
-        if (parent) {
-          colorToTint.r += parent.color.r;
-          colorToTint.g += parent.color.g;
-          colorToTint.b += parent.color.b;
-          colorToTint.a += parent.color.a;
-          colorsAdded += 1;
-        }
-        if (leftCousin) {
-          colorToTint.r += leftCousin.color.r;
-          colorToTint.g += leftCousin.color.g;
-          colorToTint.b += leftCousin.color.b;
-          colorToTint.a += leftCousin.color.a;
-          colorsAdded += 1;
-        }
-        if (rightCousin) {
-          colorToTint.r += rightCousin.color.r;
-          colorToTint.g += rightCousin.color.g;
-          colorToTint.b += rightCousin.color.b;
-          colorToTint.a += rightCousin.color.a;
-          colorsAdded += 1;
-        }
-        if(colorsAdded != 0) {
-          colorToTint.r /= colorsAdded;
-          colorToTint.g /= colorsAdded;
-          colorToTint.b /= colorsAdded;
-          colorToTint.a /= colorsAdded;
-
-          let randomMultiplier = Math.random()>0.99 ? 57 : 1;
-          let consistentMultiplier = 7;
-          let multiplierSum = randomMultiplier + consistentMultiplier;
-
-          let red =
-            randomMultiplier * color.r
-            + consistentMultiplier * colorToTint.r;
-          let green =
-            randomMultiplier * color.g
-            + consistentMultiplier * colorToTint.g;
-          let blue =
-            randomMultiplier * color.b
-            + consistentMultiplier * colorToTint.b;
-          let alpha =
-            randomMultiplier * color.a
-            + consistentMultiplier * colorToTint.a;
-
-          red = Math.floor(red/multiplierSum);
-          green = Math.floor(green/multiplierSum);
-          blue = Math.floor(blue/multiplierSum);
-          alpha = Math.floor(alpha/multiplierSum);
-
-          color.r = red;
-          color.g = green;
-          color.b = blue;
-          color.a = alpha;
-        }
-
         column.boxes.push({
           position: position,
           color: color,
@@ -203,6 +111,7 @@ export default {
     },
 
     addBox(position, color) {
+      console.log("add box");
       var newBox = new BoxClass({
         propsData: {
           position: position,
@@ -224,6 +133,7 @@ export default {
   },
 
   async mounted() {
+    console.log("Hello, world!");
     this.baseElement = document.getElementById('animation-base');
     await this.resizedWindow();
     this.renderLoop();
@@ -246,16 +156,9 @@ function getRandomElements(arr, n) {
 </script>
 
 <template>
-  <div id="animation-base">
+  <div id="animation-base" style="
+    position: absolute,
+    left: 50vw;
+  ">
   </div>
 </template>
-
-<style scoped>
-#animation-base {
-  position: absolute;
-  left: 50vw;
-  top: -20px;
-  height: calc(100vh+ 20px);
-  overflow-y: clip;
-}
-</style>
--- src/components/BackgroundBox.vue
+++ src/components/BackgroundBox.vue
@@ -16,8 +16,8 @@ export default {
       }
       return 1;
     },
-    rgbaToHex(rgb) {
-      return `#${decToTwoDigitHex(rgb.r)}${decToTwoDigitHex(rgb.g)}${decToTwoDigitHex(rgb.b)}${decToTwoDigitHex(rgb.a)}`
+    rgbToHex(rgb) {
+      return `#${decToTwoDigitHex(rgb.r)}${decToTwoDigitHex(rgb.g)}${decToTwoDigitHex(rgb.b)}`
     }
   },
 }
@@ -30,8 +30,8 @@ function decToTwoDigitHex(dec) {
 
 <template>
   <div class="box" :style="{
-    '--backgroundColor': `${rgbaToHex(color)}`,
-    '--left': `${position.x*boxSize - xDirection()*boxSize/2}px`,
+    '--backgroundColor': `${rgbToHex(color)}`,
+    '--left': `${position.x*boxSize}px`,
     '--top': `${position.y*boxSize}px`,
     '--boxSize': `${boxSize}px`
   }">
@@ -41,10 +41,8 @@ function decToTwoDigitHex(dec) {
 <style>
   .box {
     position: absolute;
-    width: calc(var(--boxSize)*9/10);
-    height: calc(var(--boxSize)*9/10);
-    border-width: calc(var(--boxSize)*1/10);
-    border-color: rgba(0, 0, 0, 0);
+    width: var(--boxSize);
+    height: var(--boxSize);
   }
 </style>
 

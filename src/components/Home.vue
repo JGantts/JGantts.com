3411103b4e225dfbdabd584c993108abbb9637ba
Jacob Gantt <gantt42@gmail.com>
2022-12-23 23:35:49 -0500

    fix more

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,11 +22,11 @@ export default {
               <p id="text04">Jacob Gantt</p>
               <ul id="links01" class="links">
                 <li class="n01">
-                  <a href="mailto:gantt42@gmail.com"><i class="fa-solid fa-envelope" />&nbsp;&nbsp;&nbsp;<span class="underline-link">gantt42@gmail.com</span></a>
+                  <a href="mailto:gantt42@gmail.com"><i class="fa-solid fa-envelope" />&nbsp;&nbsp;<span class="underline-link">gantt42@gmail.com</span></a>
                 </li><li class="n02">
-                  <a href="https://github.com/JGantts"><i class="fa-brands fa-github"></i>&nbsp;&nbsp;&nbsp;<span class="underline-link">github.com/JGantts</span></a>
+                  <a href="https://github.com/JGantts"><i class="fa-brands fa-github"></i>&nbsp;&nbsp;<span class="underline-link">github.com/JGantts</span></a>
                 </li><li class="n03">
-                  <a href="https://jgantts.com/resume/GanttJ-Resume.pdf"><i class="fa-solid fa-file-pdf"></i>&nbsp;&nbsp;&nbsp;<span class="underline-link">GanttJ-Resume.pdf</span></a>
+                  <a href="https://jgantts.com/resume/GanttJ-Resume.pdf"><i class="fa-solid fa-file-pdf"></i>&nbsp;&nbsp;<span class="underline-link">GanttJ-Resume.pdf</span></a>
                 </li>
               </ul>
             </div>

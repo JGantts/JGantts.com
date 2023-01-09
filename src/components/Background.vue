6534b4805fd47c742bc1879b35b1c68c220206d8
gantt42 <gantt42@gmail.com>
2023-01-08 21:14:23 -0500

    Speed things up

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -111,7 +111,7 @@ async function resizedWindow() {
     gaussianObjects = []
     for (let index=gaussianDistance; index < countToAdd-gaussianDistance; index++) {
       gaussianObjects.push({
-          position: gaussianSumsPosition[index] - 1 + -1*(Math.abs(index-canvasPixelElement.width*3/100))/10,
+          position: gaussianSumsPosition[index] - 1 - 3*(Math.abs(index-0.15*countToAdd))/countToAdd,
           velocity: gaussianSumsVelocity[index]/1000,
           acceleration: gaussianSumsAcceleration[index]/10000,
           jolt: gaussianSumsJolt[index]*-1/10000000,

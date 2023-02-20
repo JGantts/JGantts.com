d7703cb7d19456fa9acc8937f739331b23291b64
JGantts <contact@jgantts.com>
2023-02-20 07:23:41 +0000

    prod env works!

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,22 @@
 require('dotenv').config()
 import express, { Express, Request, Response } from 'express';
+let serveStatic = require('serve-static');
 
-const app: Express = express();
-const port = process.env.API_PORT || 3000;
+const api: Express = express()
+const port = process.env.API_PORT
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+api.get('/imgs/*', (req: Request, res: Response) => {
+  res.send(`You'd like an image, eh?`);
+})
 
-app.listen(port, () => {
+api.get('/api/*', (req: Request, res: Response) => {
+  res.send(`You'd like a function, eh?`);
+})
+
+if (process.env.APP_STATIC_FILES) {
+  api.get('/*', serveStatic(process.env.APP_STATIC_FILES, { maxAge: 30*60*1000 }))
+}
+
+api.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+})

959f75392ff85f752a6406a53c5637d6ec5fd14d
JGantts <contact@jgantts.com>
2023-02-20 17:21:13 +0000

    Add static serve

--- install-stage/index.js
+++ install-stage/index.js
@@ -5,11 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const port = process.env.API_PORT || 3000;
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+let serveStatic = require('serve-static');
+const api = (0, express_1.default)();
+const port = process.env.API_PORT;
+api.get('/imgs/*', (req, res) => {
+    res.send(`You'd like an image, eh?`);
 });
-app.listen(port, () => {
+api.get('/api/*', (req, res) => {
+    res.send(`You'd like a function, eh?`);
+});
+if (process.env.APP_STATIC_FILES) {
+    api.get('/*', serveStatic(process.env.APP_STATIC_FILES, { maxAge: 30 * 60 * 1000 }));
+}
+api.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });

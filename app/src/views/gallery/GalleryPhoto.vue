1e332652d6c366dee1631fc51d3273ea1327193c
JGantts <contact@jgantts.com>
2023-03-13 14:01:40 +0000

    Improve dehashing speed

--- app/src/views/gallery/GalleryPhoto.vue
+++ app/src/views/gallery/GalleryPhoto.vue
@@ -16,6 +16,8 @@ let getImgUri = (photoID: string) => {
   return `${import.meta.env.VITE_APP_API_ENDPOINT}/photo-library/img/${photoID}/w-${320 * window.devicePixelRatio}.jpg`
 }
 
+let scaleRatio = 15
+
 export default {
   props: [
     'photo'
@@ -23,34 +25,60 @@ export default {
   setup(props) {
 
     let widthPixels = 320
-    let heightPixels = 320 / props.photo.dimensionsRatio
+    let heightPixels = Math.ceil(320 / props.photo.dimensionsRatio)
+
+    let widthPoints = widthPixels * window.devicePixelRatio
+    let heightPoints = heightPixels * window.devicePixelRatio
+
+    let widthSmall = Math.ceil(widthPixels/scaleRatio)
+    let heightSmall = Math.ceil(heightPixels/scaleRatio)
 
     let id = props.photo.id
     let elementID = 'photo-canvas-' + id
 
     console.log(id)
 
+    //create blur
+    let blurData: any = decode(
+        props.photo.blurHash,
+        widthSmall,
+        heightSmall
+      )
+    const imageData = new ImageData(blurData, widthSmall)
+
     onMounted(async () => {
-      let blurData: any = decode(
-          props.photo.blurHash,
-          320,
-          Math.ceil(heightPixels)
-        )
 
       // @ts-ignore
-      let ctx = document.getElementById(elementID).getContext("2d")
+      let canvas = document.getElementById(elementID) as HTMLCanvasElement
+      let ctx = canvas.getContext("2d")!
 
-      const imageData = new ImageData(blurData, 320)
+      //draw blur @ small scale
       ctx.putImageData(imageData, 0, 0)
 
-      ctx.scale(1/window.devicePixelRatio, 1/window.devicePixelRatio)
-      let tempImage = new Image()
-      tempImage.onload = () => {
-        console.log('why')
-        ctx.drawImage(tempImage, 0, 0)
+      //draw blur @ large scale
+      let tempImageBlur = new Image()
+      tempImageBlur.onload = () => {
+        //ctx.scale(scaleRatio*window.devicePixelRatio, scaleRatio*window.devicePixelRatio)
+        ctx.drawImage(
+          tempImageBlur,
+          0, 0,
+          widthSmall-1, heightSmall-1,
+          0, 0,
+          widthPoints, heightPoints
+        )
       }
-      console.log('when')
-      tempImage.src = getImgUri(id)
+      tempImageBlur.src = canvas.toDataURL() 
+
+      //reset scale
+      //ctx.scale(1/scaleRatio, 1/scaleRatio)
+      ctx.scale(1/window.devicePixelRatio, 1/window.devicePixelRatio)
+
+      //draw final image
+      let tempImageFinal = new Image()
+      tempImageFinal.onload = () => {
+        ctx.drawImage(tempImageFinal, 0, 0)
+      }
+      tempImageFinal.src = getImgUri(id)
     });
  
     onUnmounted(async () => {

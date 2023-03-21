60df57a89e81a9154d91cb651ca9632b74387c29
JGantts <contact@jgantts.com>
2023-03-21 23:16:07 +0000

    fixes

--- CMS/src/api/post/content-types/post/lifecycles.ts
+++ CMS/src/api/post/content-types/post/lifecycles.ts
@@ -1,17 +1,6 @@
-function makeid(length: number) {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
-}
-
 module.exports = {
   beforeCreate: async (event) => {
+    const { makeid } = require('../../../common')
     const { data } = event.params
     data.UUID = makeid(20)
   }

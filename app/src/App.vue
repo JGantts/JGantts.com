8a3d0fb1d17a6594e5b3a67180084419c375d735
Jacob Gantt <gantt42@gmail.com>
2024-02-11 17:15:49 -0500

    Update copyright year

--- app/src/App.vue
+++ app/src/App.vue
@@ -153,7 +153,7 @@ function checkDarkMode(mediaMatch: any) {
             <div class="wrapper">
               <div class="inner">
                 <p id="text05">I write software!</p>
-                <p id="text07">© 2023 Jacob Gantt</p>
+                <p id="text07">© 2024 Jacob Gantt</p>
               </div>
             </div>
           </div>

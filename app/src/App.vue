6b31ad0fb0d4699a23e927a00b387d059b65dce9
Jacob Gantt <contact@jgantts.com>
2024-02-26 21:25:06 -0500

    last thing refactored... ? maybe

--- app/src/App.vue
+++ app/src/App.vue
@@ -35,66 +35,102 @@ function reloadBackgound() {
 
 <template>
   <div id="app">
-    <div id="background-holder">
-      <div id="wrapper">
-        <Island cornerRadius="1.5rem">
-          <VStack padding="0.75rem 1.25rem" spacing="0">
-            <h1>
-              <span id="text01-accent">JGantts</span>
-              <span id="text01-dot">.</span>
-              <span id="text01">com</span>
-            </h1>
-            <p id="text02">Welcome, all.</p>
-          </VStack>
-        </Island>
-        <Island cornerRadius="1.5rem">
+    <div id="box">
+      <Background id="background" ref="{backgroundRef}" />
+      <div id="content">
+        <VStack padding="1.25rem" spacing="0.5rem">
+          <Island cornerRadius="1.5rem">
+            <VStack padding="0.75rem 1.25rem" spacing="0">
+              <h1>
+                <span id="text01-accent">JGantts</span>
+                <span id="text01-dot">.</span>
+                <span id="text01">com</span>
+              </h1>
+              <p id="text02">Welcome, all.</p>
+            </VStack>
+          </Island>
+          <Island cornerRadius="1.5rem">
+            <DStack
+              :breakpoint="Breakpoint._2_M"
+              padding="1rem"
+              hSpacing="1rem"
+              vSpacing="1rem"
+            >
+              <VStack>
+                <p id="text03">Contact me about<br />software:</p>
+                <p id="text04">Jacob Gantt</p>
+                <Links />
+              </VStack>
+              <p id="text06" style="max-width: 340px">
+                Looking for clients and collaborators.<br />
+                I&#039;m a developer with years of professional and personal
+                experience; from improving enterprise solutions to fuzz testing
+                code parsers; game mods, websites; APIs and UX; C#, JavaScript;
+                Swift, Node; I love it all!
+              </p>
+            </DStack>
+          </Island>
+          <!-- <NavBar /> -->
           <DStack
             :breakpoint="Breakpoint._2_M"
-            padding="1rem"
+            padding="0"
             hSpacing="1rem"
             vSpacing="1rem"
           >
-            <VStack>
-              <p id="text03">Contact me about<br />software:</p>
-              <p id="text04">Jacob Gantt</p>
-              <Links />
-            </VStack>
-            <p id="text06" style="max-width: 340px">
-              Looking for clients and collaborators.<br />
-              I&#039;m a developer with years of professional and personal
-              experience; from improving enterprise solutions to fuzz testing
-              code parsers; game mods, websites; APIs and UX; C#, JavaScript;
-              Swift, Node; I love it all!
-            </p>
+            <ExpandedView>
+              <ReplayButton style="visibility: hidden"/>
+            </ExpandedView>
+            <Island cornerRadius="0.5rem">
+              <VStack padding="0.25rem 0.75rem">
+                <p id="text05">I write software!</p>
+                <p id="text07">© 2024 Jacob Gantt</p>
+              </VStack>
+            </Island>
+            <ReplayButton @click="reloadBackgound" />
           </DStack>
-        </Island>
-        <!-- <NavBar /> -->
-        <DStack
-          :breakpoint="Breakpoint._2_M"
-          padding="0"
-          hSpacing="1rem"
-          vSpacing="1rem"
-        >
-          <ExpandedView>
-            <ReplayButton style="visibility: hidden"/>
-          </ExpandedView>
-          <Island cornerRadius="0.5rem">
-            <VStack padding="0.25rem 0.75rem">
-              <p id="text05">I write software!</p>
-              <p id="text07">© 2024 Jacob Gantt</p>
-            </VStack>
-          </Island>
-          <ReplayButton @click="reloadBackgound" />
-        </DStack>
-      </div>
-      <div id="background">
-        <Background ref="{backgroundRef}" />
+        </VStack>
       </div>
     </div>
   </div>
 </template>
 
 <style>
+#app {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: auto;
+}
+
+#box {
+  position: relative;
+  width: 100%;
+  max-width: 100vw;
+  z-index: 2;
+  overflow: visible;
+}
+
+#background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+}
+
+#content {
+  position: relative;
+  max-width: 100vw;
+  max-height: 100vh;
+  z-index: 3;
+}
+
 .main {
   background-color: var(--backgroundAppBase);
 }

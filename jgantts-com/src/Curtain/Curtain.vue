d66cdca99e135ad0396a7b7911f45aab23b234ec
gantt42 <gantt42@gmail.com>
2025-12-03 20:44:14 -0500

    Update Curtain.vue

--- jgantts-com/src/Curtain/Curtain.vue
+++ jgantts-com/src/Curtain/Curtain.vue
@@ -98,7 +98,7 @@ async function initializeBackground() {
 
   doneAnimatingCurtain = false
 
-  const ratio = 0.5 //window.devicePixelRatio || 1;
+  const ratio = window.devicePixelRatio || 1;
   if (canvasElement.width != width) {
     canvasElement.width = width * ratio;
     canvasElement.height = height * ratio;

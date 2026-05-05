0e85ee164829abe76605798aa5d93003a7c91e6c
Jacob Gantt <contact@jgantts.com>
2026-05-05 16:48:18 -0400

    Update make_all_tiles.py

--- python/make_all_tiles.py
+++ python/make_all_tiles.py
@@ -17,10 +17,12 @@ Builds:
 
 import argparse
 import json
+import os
 import shutil
 import subprocess
 import sys
 from pathlib import Path
+import tempfile
 
 TILER = Path("./tile_pm.py").resolve()
 DEMMER = Path("./tile_dem.py").resolve()
@@ -41,14 +43,23 @@ def normalize_in_file_path(path):
         return p
     return SRC_DIR / p
 
-def make_normalize_out_file_path(args):
-    def normalize_out_file_path(path):
+def make_out_file_path(args):
+    def out_file_path():
         base = OUTPUT_DIR(args.dev)
-        p = Path(path)
-        if p.is_absolute():
-            return p
-        return base / p
-    return normalize_out_file_path
+        return base
+    return out_file_path
+
+def make_temp_out_dir():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        def temp_out_file_path(path):
+            base = temp_dir
+            p = Path(path)
+            if p.is_absolute():
+                return p
+            return base / p
+        def get_temp_dir():
+            return temp_dir
+        return (temp_out_file_path, get_temp_dir)
 
 def build(input_file, output_file, bounds, minzoom, maxzoom):
     west, south, east, north = bounds
@@ -79,8 +90,10 @@ def main():
 
     args = p.parse_args()
 
-    normalize_out_file_path = make_normalize_out_file_path(args)
-    
+    out_file_path = make_out_file_path(args)
+    (temp_out_file_path, get_temp_dir) = make_temp_out_dir()
+
+
     regions_json_in = Path(REGIONS_JSON_IN).resolve()
     regions_json_out = Path(REGIONS_JSON_OUT(args.dev)).resolve()
 
@@ -93,14 +106,6 @@ def main():
         sys.exit(1)
 
     regions = json.loads(regions_json_in.read_text(encoding="utf-8"))
-
-
-    print("\n=== COPYING REGIONS GEOJSON ===")
-    #copy input regions geojson to output
-    regions_json_out.parent.mkdir(parents=True, exist_ok=True)
-    regions_json_out.write_text(regions_json_in.read_text(encoding="utf-8"), encoding="utf-8")
-
-
 
     # -------------------------------------------------
     # WORLD
@@ -141,7 +146,7 @@ def main():
                 return
 
             input_file = normalize_in_file_path(relative_file).with_suffix(".png")
-            output_file = normalize_out_file_path(relative_file).with_suffix(".pmtiles")
+            output_file = temp_out_file_path(relative_file).with_suffix(".pmtiles")
 
             print(input_file)
 
@@ -155,7 +160,7 @@ def main():
 
             if layer.get("hasDark"):
                 input_file_dark = normalize_in_file_path(relative_file + "-dark").with_suffix(".png")
-                output_file_dark = normalize_out_file_path(relative_file + "-dark").with_suffix(".pmtiles")
+                output_file_dark = temp_out_file_path(relative_file + "-dark").with_suffix(".pmtiles")
 
                 print(input_file_dark)
 
@@ -183,18 +188,18 @@ def main():
             elif layerType == "single":
                 # copy png file
                 input_file = normalize_in_file_path(layer["imageUrl"]).with_suffix(".png")
-                output_file = normalize_out_file_path(layer["imageUrl"]).with_suffix(".png")
+                output_file = temp_out_file_path(layer["imageUrl"]).with_suffix(".png")
                 shutil.copy(input_file, output_file)
                 if layer.get("hasDark"):
                     # copy dark png file
                     input_file_dark = normalize_in_file_path(layer["imageUrl"]+"-dark").with_suffix(".png")
-                    output_file_dark = normalize_out_file_path(layer["imageUrl"]+"-dark").with_suffix(".png")
+                    output_file_dark = temp_out_file_path(layer["imageUrl"]+"-dark").with_suffix(".png")
                     shutil.copy(input_file_dark, output_file_dark)
         
 
     relative_file = WORLD_ERODED_IN
     input_file = normalize_in_file_path(relative_file).with_suffix(".png")
-    output_dir = normalize_out_file_path("height-tiles")
+    output_dir = temp_out_file_path("height-tiles")
 
     print(f"\n=== HEIGHT {region_id} ===")
 
@@ -211,6 +216,20 @@ def main():
         str(east),
         str(north)
     ])
+
+    print("\n=== COPYING COMPILED IMAGES ===")
+    #copy results
+    temp_dir = get_temp_dir()
+    output_dir = out_file_path()
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir) 
+    shutil.copytree(temp_dir, output_dir)
+
+    print("\n=== COPYING REGIONS GEOJSON ===")
+    #copy input regions geojson to output
+    regions_json_out.parent.mkdir(parents=True, exist_ok=True)
+    regions_json_out.write_text(regions_json_in.read_text(encoding="utf-8"), encoding="utf-8")
+
 
     print("\nDONE")
 

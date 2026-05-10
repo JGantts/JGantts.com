bceeede4377570f8d6be80ab55105f1336c1719b
Jacob Gantt <contact@jgantts.com>
2026-05-10 10:13:55 -0400

    shoot for the moon!

--- jgantts-com/src/main.ts
+++ jgantts-com/src/main.ts
@@ -8,4 +8,7 @@ const app = createApp(App)
 import router from './router'
 app.use(router)
 
+import vuetify from './plugins/vuetify'
+app.use(vuetify)
+
 app.mount('#app')
---
+++ jgantts-com/src/plugins/vuetify.ts
@@ -0,0 +1,7 @@
+// Import Vue and Vuetify components
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'  // Import Vuetify styles
+
+export default createVuetify({
+  // Any configuration options go here
+})

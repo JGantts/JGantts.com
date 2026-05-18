6cd642732759613eb3578e5f36ddd48774e6ae4d
Jacob Gantt <contact@jgantts.com>
2026-05-18 08:36:36 -0400

    Update maps.ts

--- jgantts-com/src/views/kovyalo/maps/maps.ts
+++ jgantts-com/src/views/kovyalo/maps/maps.ts
@@ -146,6 +146,8 @@ async function initMap(mapEl: HTMLElement | null, dev: boolean = false): Promise
       bearing: settings.bearing,
     })
 
+    //test
+
     if (dev) {
       mapTemp!.getCanvas().style.cursor = 'crosshair'
     }
